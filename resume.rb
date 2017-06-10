e88c4d9a9eac4a819f29d289729af04af5ad759a
Evans Tucker <evans.tucker@gmail.com>
2017-06-10 10:02:00 -0700

    Added javascript to make date calculation live instead of staticly generated during resume creation.

--- resume.rb
+++ resume.rb
@@ -7,12 +7,43 @@ require 'yaml'
 
 resume = YAML.load_file('decrypted_resume.yaml')
 
-# HTML Head and Style
 output = <<~END
   <!DOCTYPE html>
   <html lang="en">
     <head>
       <meta charset="utf-8">
+      <script type="text/javascript">
+        function calculateYears() {
+          "use strict";
+          var yearsElements = document.getElementsByClassName("years");
+          var i;
+          var startDate;
+          var endDate;
+          var startDateUTC;
+          var endDateUTC;
+          var diffInYears;
+          for (i = 0; i < yearsElements.length; i += 1) {
+            startDate = new Date(yearsElements[i].dataset.startDate);
+            if (yearsElements[i].dataset.endDate === "") {
+              endDate = new Date();
+            } else {
+              endDate = new Date(yearsElements[i].dataset.endDate);
+            }
+            // Convert Dates to milliseconds so we can do math.
+            startDateUTC = Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+            endDateUTC = Date.UTC(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
+            diffInYears = ((endDateUTC - startDateUTC) / 1000 / 60 / 60 / 24 / 365).toFixed(2);
+            // LLLLLEEEEEEERRROOOOOOOOYYYYYYNNNJEEEENNKKIINNSSS!
+            // Set diffInYears to something ending in ".33" to test.
+            //diffInYears = 1.33
+            if ( /\\.33/.test(diffInYears) ) {
+              yearsElements[i].innerHTML = '(<a href="https://www.youtube.com/watch?v=hooKVstzbz0" title="... point 33 - repeating of course ...">' + diffInYears + '</a> years)';
+            } else {
+              yearsElements[i].textContent = " (" + diffInYears + " years)";
+            }
+          }
+        }
+      </script>
       <style>
         a {
           color: black;
@@ -25,8 +56,10 @@ output = <<~END
         body {
           font-family: sans-serif;
         }
+	/* IE displays bullets differently, so I decided to just use Unicode
+             bullets at the beginning of my list items. */
         li {
-          list-style: none; /* IE displays bullets differently */
+          list-style: none;
         }
         p {
           padding-left: 2%;
@@ -71,7 +104,7 @@ output = <<~END
       </style>
       <title>#{resume['contact']['name']}'s Resume</title>
     </head>
-    <body>
+    <body onload="calculateYears()">
   END
 
 # Contact Info and Biography
@@ -96,24 +129,13 @@ resume['experience'].each do |job|
     end
     if job['end_date'].nil?
       duration = "#{job['start_date']} to Present"
-      job['end_date'] = DateTime.now
     else
       duration = "#{job['start_date']} to #{job['end_date']}"
     end
-    years = "%0.2f" % ((job['end_date'] - job['start_date']).to_f / 365)
-    # LLLLLEEEEEEERRROOOOOOOOYYYYYYNNNJEEEENNKKIINNSSS!
-    if years.to_s.chars.last(2).join == '33'
-      duration << " (<a href=\"https://www.youtube.com/watch?v=hooKVstzbz0\" title=\"repeating of course\">#{years}</a> years)"
-    else
-      duration << " (#{years} years)"
-    end
+    duration << "<span class=\"years\" data-start-date=\"#{job['start_date']}\" data-end-date=\"#{job['end_date']}\"></span>"
     output << <<~END
-          <div class="left">
-            #{job['title']} at <a href="#{job['url']}">#{job['company']}</a> (#{job['location']})
-          </div>
-          <div class="right">
-            #{duration}
-          </div>
+          <div class="left">#{job['title']} at <a href="#{job['url']}">#{job['company']}</a> (#{job['location']})</div>
+          <div class="right">#{duration}</div>
           <div class="clear">
       END
     if description_entity == 'li'
@@ -134,12 +156,8 @@ output << "    <div class=\"headline\">Education</div>\n"
 resume['education'].each do |institution|
   if institution['tags'].to_s.include? 'college'
     output << <<~END
-          <div class="left">
-            <a href="#{institution['url']}">#{institution['name']}</a>
-          </div>
-          <div class="right">
-            #{institution['degree']} in #{institution['major']}
-          </div>
+          <div class="left"><a href="#{institution['url']}">#{institution['name']}</a></div>
+          <div class="right">#{institution['degree']} in #{institution['major']}</div>
           <div class="clear">
             <ul>
       END
@@ -153,6 +171,7 @@ resume['education'].each do |institution|
   end
 end
 
+# Footer
 output << <<~END
       <div class="generated">This resume isn't a Word doc or a PDF - it's code! You can see the source here: <a class="generated" href="https://gitlab.com/evanstucker/resume">https://gitlab.com/evanstucker/resume</a></div>
     </body>
@@ -164,4 +183,6 @@ file_name = "#{resume['contact']['name']}'s Resume.html"
 f = File.new("#{file_name}", 'w+')
 f.write(output)
 f.close
-system( "tidy -i -m -w \"#{file_name}\"" )
+
+# This mangles things, but I wish it wouldn't...
+#system( "tidy -i -m -utf8 -w \"#{file_name}\"" )
